3018e72eb15b7d2393381b156efaf94e7c12c817
Yuvalbuk07 <boxsa1205@gmail.com>
2025-03-09 21:10:32 +0200

    הוספת קובץ AppNavigation.js

---
+++ AppNavigation.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// ייבוא מסכים
+import SplashScreen from './screens/SplashScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import TravellerTypeScreen from './screens/TravellerTypeScreen';
+import TripInfoScreen from './screens/TripInfoScreen';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigation = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Splash"
+                screenOptions={{
+                    headerShown: false, // להסתיר כותרת בכל המסכים
+                }}
+            >
+                <Stack.Screen name="Splash" component={SplashScreen} />
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Register" component={RegisterScreen} />
+                <Stack.Screen name="TravellerType" component={TravellerTypeScreen} />
+                <Stack.Screen name="TripInfo" component={TripInfoScreen} />
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default AppNavigation;
